85b0fcf7f7156242268307ea1cffd234d20c16b4
ehgus964 <ehgus964@gmail.com>
2023-07-22 09:53:12 +0900

    0722 lv2 string

---
+++ programmers_lv2/JadenCase.py
@@ -0,0 +1,9 @@
+def solution(s):
+    answer = ''
+    words = s.split(" ")    
+    for idx, word in enumerate(words):
+        words[idx] = word[:1].upper() + word[1:].lower()
+        # words[idx] = word[0].upper() + word[1:].lower()        
+    return ' '.join(words)
+
+# 5번째줄 6번째줄 차이를 모르겠음....... 알면 추가하기
