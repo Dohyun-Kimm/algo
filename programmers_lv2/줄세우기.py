ee9c8decc858d1402a4c3c5868d945e6fd7af04d
ehgus964 <ehgus964@gmail.com>
2023-12-13 11:32:17 +0900

    1213 lv2 factorial need retry

---
+++ programmers_lv2/줄세우기.py
@@ -0,0 +1,21 @@
+def factorial(n):
+    if n < 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+
+def solution(n, k):
+    answer = []
+    num_list = [i for i in range(1, n + 1)]
+
+    while n != 0:
+        num_case = factorial(n - 1)
+        idx = k // num_case
+        k = k % num_case
+        if k == 0:
+            answer.append(num_list.pop(idx - 1))
+        else:
+            answer.append(num_list.pop(idx))
+        n -= 1
+    return answer
